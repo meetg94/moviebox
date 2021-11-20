66bf684ae3f3620c6f72d9344d2631957576dc17
bakedegg <meetguleria94@gmail.com>
2021-11-19 22:45:03 -0800

    v3

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './Assets/logo.png';
+import Movie from './Movie';
 
 function Header() {
+
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const [movies, setMovies] = useState([]);
+
+useEffect(() => {
+    fetch(FEATURED_API)
+        .then((res) => res.json())
+        .then((data) => {
+            setMovies(data.results);
+        });
+}, []);
+
+
     return (
-        <div className="title-logo">
-
-            <h1 className="title">Popcorn Flicks</h1>
-            <img className="logo" src={logo}></img>
-
-        </div>
+        <>
+            <div className="title-logo">
+                <h1 className="title">Popcorn Flicks</h1>
+                <img className="logo" src={logo}></img>
+            </div>
+            <div className="movie-container">
+                {movies.length > 0 && movies.map((movie) => <Movie key={movie.id}
+            {...movie}/>)}
+            </div>
+        </>
     )
 }
 
